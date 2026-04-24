889745ba48733253a840daa598e33eedb3572986
BR-HY <1580745927@qq.com>
2026-04-24 11:26:51 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://ehfohlomvptvhnzodset.supabase.co",
+  supabaseAnonKey: "sb_publishable_PteTHDCFWQrX2tp85qnq4g_MrLPQgQz"
 };
